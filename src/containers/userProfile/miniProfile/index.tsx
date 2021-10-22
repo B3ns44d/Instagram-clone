656af436ebd3230e95c29340017be20c6882f1e5
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-10-22 15:06:29 +0100

    feat: mini profile with api

--- src/containers/userProfile/miniProfile/index.tsx
+++ src/containers/userProfile/miniProfile/index.tsx
@@ -1,27 +1,32 @@
 /* eslint-disable @next/next/no-img-element */
 import UserNameText from '@components/UserNameText';
 import type { FC, ReactElement } from 'react';
-import { signOut } from 'next-auth/client';
+import { signOut, useSession } from 'next-auth/client';
+import { generateUsername } from '@shared/utils';
 
-const MiniProfile: FC = (): ReactElement => (
-	<div className="flex items-center justify-between mt-14 ml-10">
-		<img
-			src="https://k.top4top.io/p_21165x5vn1.jpeg"
-			alt="youssef.py3's profile"
-			className="rounded-full border p-[2px] w-16 h-16"
-		/>
-		<div className="flex-1 mx-4">
-			<UserNameText userName="youssef.py3" />
-			<h3 className="text-sm text-gray-500">검은 색 | black</h3>
+const MiniProfile: FC = (): ReactElement => {
+	const [session] = useSession();
+	const { image, name } = session?.user;
+	return (
+		<div className="flex items-center justify-between mt-14 ml-10">
+			<img
+				src={image}
+				alt={`${name} profile`}
+				className="rounded-full border p-[2px] w-16 h-16"
+			/>
+			<div className="flex-1 mx-4">
+				<UserNameText userName={generateUsername(name)} />
+				<h3 className="text-sm text-gray-500">{name}</h3>
+			</div>
+			<button
+				type="submit"
+				className="text-blue-400 text-sm font-semibold"
+				onClick={() => signOut()}
+			>
+				Sign out
+			</button>
 		</div>
-		<button
-			type="submit"
-			className="text-blue-400 text-sm font-semibold"
-			onClick={() => signOut()}
-		>
-			Sign out
-		</button>
-	</div>
-);
+	);
+};
 
 export default MiniProfile;
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,11 +1,18 @@
-export const createLinkPath = (path: string): string => {
+const createLinkPath = (path: string): string => {
 	if (path === undefined) {
 		return '/';
 	}
 	return path.startsWith('/') ? path : `/${path}`;
 };
 
-export const nextAuthProviderConfig = {
+const nextAuthProviderConfig = {
 	clientMaxAge: 0,
 	keepAlive: 0,
 };
+
+const generateUsername = (name: string): string => {
+	const username = name.toLowerCase().replace(/\s/g, '_');
+	return username.length > 0 ? username : 'user';
+};
+
+export { generateUsername, createLinkPath, nextAuthProviderConfig };
